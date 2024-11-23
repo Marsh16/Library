5ef37d0012a29724dffa553369d92a076d303505
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2024-11-23 11:55:25 +0700

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Marsha Likorawung on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
